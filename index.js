7b51d4ff1ea69d70550b932ed6ebcd5ff08de755
raecello <37798815+raecello@users.noreply.github.com>
2018-10-16 23:17:21 +1100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //console.log(breq.queryResult.action);
 app.post('/get-ISS-details', (req, res) => {
-    console.log(req.body);
+
     if (req.body.queryResult.action == 'Get-lat-long') {
         
         const reqUrl = encodeURI(`http://api.open-notify.org/iss-now.json`);
@@ -39,6 +39,92 @@ app.post('/get-ISS-details', (req, res) => {
             });
         });
 }
+   
+if (req.body.queryResult.action == 'who-is-onboard') {
+    
+    const reqUrl = encodeURI(`http://api.open-notify.org/astros.json`);
+    http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            var astronauts = JSON.parse(completeResponse);
+            astronauts = astronauts.people;
+            var dataToSend = "The astronauts currently on the ISS are: "            
+            for (var i = 0; i < astronauts.length; i++) {
+                if (i == astronauts.length - 1) {
+                    dataToSend += "and ";
+                }
+                dataToSend += astronauts[i].name;
+                if (i == astronauts.length - 1) {
+                    dataToSend += ".";
+                } else {
+                    dataToSend += ", ";
+                }                  
+            }
+            return res.json({
+                fulfillmentText: dataToSend,
+                source: 'who-is-onboard'
+            });
+        });
+    }, (error) => {
+        return res.json({
+            fulfillmentText: 'Something went wrong!',
+            source: 'who-is-onboard'
+        });
+    });
+    }
+
+    // needs latitude and longitude objects to both be in place
+if (req.body.queryResult.action == 'whenOverHead') {
+
+    const reqUrl = encodeURI(`http://api.open-notify.org/iss-pass.json?lat=${req.body.queryResult.parameters.latitude}&lon=${req.body.queryResult.parameters.longitude}`);
+    http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            var nextPassTime = JSON.parse(completeResponse);
+            try {
+            nextPassTime = nextPassTime.response[0].risetime;
+            }
+            catch (e) {
+                return res.json({
+                    fulfillmentText: "Please choose a latitude between -90.0 and 90.0 and a longitude between -180.0 and 180.0.",
+                    source: 'whenOverHead' 
+                })
+            }
+            var dataToSend = `The ISS will next be overhead at this location on ${getUTCTime(nextPassTime)}`;          
+            return res.json({
+                fulfillmentText: dataToSend,
+                source: 'whenOverHead'
+            });
+        });
+    }, (error) => {
+        return res.json({
+            fulfillmentText: 'Something went wrong!',
+            source: 'whenOverHead'
+        });
+    });
+    }
+  
+    // Helper Functions
+    function getUTCTime(timestamp) {
+
+        var pubDate = new Date(timestamp*1000);  
+        var weekday=new Array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
+        var monthname=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+        var formattedDate = pubDate.getHours() + ':' + singleDigitPad(pubDate.getMinutes()) + ' '  + weekday[pubDate.getDay()] + ' '
+        + monthname[pubDate.getMonth()] + ' '
+        + pubDate.getDate() + ', ' + pubDate.getFullYear() + ' UTC'
+        return formattedDate; 
+    }
+
+    function singleDigitPad(mins) {
+        return ('0' + mins).slice(-2); 
+    }
 });
 app.listen(process.env.PORT || 5000), () => {
     console.log("Server is up and running...");
